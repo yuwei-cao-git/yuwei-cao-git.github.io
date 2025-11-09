5ef197b7d83b733da40fe5d0e11bc4244dcbea7a
yuwei-cao-git <yuwei.cao94@gmail.com>
2025-11-09 11:51:18 +0000

    Deploying to gh-pages from @ yuwei-cao-git/yuwei-cao-git.github.io@07b0fe14fdb84e40e528761efd0e0bd91d2ef764 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -429,8 +429,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-our-new-paper-enhancing-tree-species-composition-mapping-using-sentinel-2-and-multi-seasonal-deep-learning-fusion-is-now-published-in-the-international-journal-of-remote-sensing",
+          title: '🎉 Our new paper “Enhancing tree species composition mapping using Sentinel-2 and multi-seasonal...',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
