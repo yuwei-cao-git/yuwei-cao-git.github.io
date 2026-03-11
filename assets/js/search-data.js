98d359bd95252e2fe4b0e34cd1a6c68cf2d3f684
yuwei-cao-git <yuwei.cao94@gmail.com>
2026-03-11 22:31:16 +0000

    Deploying to gh-pages from @ yuwei-cao-git/yuwei-cao-git.github.io@bc891b646cbb72e283433309f62606a4cfd0098a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -333,7 +333,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/assets/pdf/example_pdf.pdf";
+          window.location.href = "/assets/pdf/CV_YUWEI_CAO.pdf";
         
       },
     },{id: "post-a-post-with-diagrams",
